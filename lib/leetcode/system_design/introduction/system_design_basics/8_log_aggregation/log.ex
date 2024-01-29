7bf19ef9e8aba1d7846fdb93dc1a28b16c9ca7b5
Gustavo Oliveira <g.92oliveira@gmail.com>
2024-01-29 15:08:19 -0300

    Refactor log aggregation code

--- lib/leetcode/system_design/introduction/system_design_basics/8_log_aggregation/log.ex
+++ lib/leetcode/system_design/introduction/system_design_basics/8_log_aggregation/log.ex
@@ -1,16 +1,57 @@
 defmodule Leetcode.SystemDesign.Introduction.SystemDesignBasics.LogAggregation.Log do
   def init_(information, infomation_2) do
-    information
-    |> Enum.zip(infomation_2)
-    |> Enum.reduce(%{}, fn {event, params}, acc ->
-      cond do
-        "LogAggregator" == event ->
-          create_machines(params)
+    {r, m} =
+      a =
+      information
+      |> Enum.zip(infomation_2)
+      |> Enum.reduce({[], %{}}, fn {event, params}, {logs, machines} = acc ->
+        cond do
+          "LogAggregator" == event ->
+            machines = create_machines(params)
+            logs = logs ++ [nil]
+            {logs, machines}
 
-        true ->
-          acc
-      end
-    end)
+          "pushLog" == event ->
+            [log_id, machine_id, service_id, message] = params
+
+            service_logs = machines[machine_id][service_id]
+
+            machines =
+              Map.put(
+                machines,
+                machine_id,
+                Map.put(machines[machine_id], service_id, service_logs ++ [{log_id, message}])
+              )
+
+            logs = logs ++ [nil]
+            {logs, machines}
+
+          "getLogsFromMachine" == event ->
+            IO.inspect(params, label: "pump")
+
+            r =
+              Enum.map(params, fn machine ->
+                machine = Map.get(machines, machine)
+                services = Map.to_list(machine)
+
+                Enum.reduce(services, [], fn {_service_id, logs}, acc ->
+                  acc ++ Enum.map(logs, fn {log_id, _m} -> log_id end)
+                end)
+                |> List.flatten()
+              end)
+
+            logs = logs ++ r
+            {logs, machines}
+
+          "getLogsOfService" == event ->
+            acc
+
+          true ->
+            acc
+        end
+      end)
+
+    r
   end
 
   defp create_machines([machines, services]) do
