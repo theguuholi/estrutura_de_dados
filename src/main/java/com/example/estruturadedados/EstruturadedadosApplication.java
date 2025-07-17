f373677e65f4b75c7fc76fec991484d226215742
Gustavo <gustavo.oliveira@stord.com>
2025-07-17 07:00:38 -0300

    first commit

---
+++ src/main/java/com/example/estruturadedados/EstruturadedadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.estruturadedados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstruturadedadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstruturadedadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estruturadedados/EstruturadedadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estruturadedados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstruturadedadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
